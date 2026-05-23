c33979e0d664666ee484c686e9e2839b390b7f1d
gdc-agency <info.globaldigitalcare@gmail.com>
2026-05-23 06:11:58 +0500

    some adjustments

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 
 const {
-  getPendingRequests,  
+  getPendingRequests,   
   approveRequest,
   rejectRequest,
   importCsvController,
